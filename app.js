6e71ddcb021aceeae5333a58c3a2fcf7267ac324
Jufebrown <jufebrown@gmail.com>
2017-03-28 15:00:17 -0500

    added routes

--- app.js
+++ app.js
@@ -2,5 +2,18 @@
 
 const express = require('express')
 const app = express()
+const routes = require(`./routes/`)
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + 'public'))
+
+app.get('/home', (req, res, next) => {
+  res.sendFile(join(__dirname, `../public`, `index.html`))
+})
+
+app.get('/see-our-chickens', (req, res, next) => {
+  res.sendFile(join(__dirname, `../public`, `chickens.html`))
+})
+
+app.get('/see-our-eggs', (req, res, next) => {
+  res.sendFile(join(__dirname, `../public`, `eggs.html`))
+})
